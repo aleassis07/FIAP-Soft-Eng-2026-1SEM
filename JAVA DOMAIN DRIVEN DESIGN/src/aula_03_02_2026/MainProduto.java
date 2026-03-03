1e964c65a1869760cc78ea9cd366ea2d4c951caf
Alexandre FIAP GITHUB <eidos.clouds@gmail.com>
2026-03-03 09:23:19 -0300

    fix: correção de sintaxe e edição de objetos

--- JAVA DOMAIN DRIVEN DESIGN/src/aula_03_02_2026/MainProduto.java
+++ JAVA DOMAIN DRIVEN DESIGN/src/aula_03_02_2026/MainProduto.java
@@ -5,22 +5,33 @@ public class MainProduto {
 
         System.out.println("MainProduto");
 
-        // Objeto
+        // Criando objetos
         Produto produto1 = new Produto();
-
         produto1.codigo = 10;
         produto1.nome = "Fandangos";
         produto1.preco = 4.99;
         produto1.tamanho = 1;
         produto1.peso = 0.50;
         produto1.estoque = 3;
+        produto1.aumentarPreco(0.1);
+        System.out.println("Preço atual do " + produto1.nome + ": " + "R$ " + produto1.preco);
 
-        produto1.aumentoPreco(0.1);
+        Produto produto2 = new Produto();
+        produto2.codigo = 23;
+        produto2.nome = "Pc Gamer Ryzen 7 32GB RAM 1TB";
+        produto2.preco = 10000.00;
+        produto2.peso = 1000.0;
+        produto2.estoque = 5;
+        produto2.diminuirPreco(2000);
+        System.out.println("Preço atual do " + produto2.nome + ": " + "R$ " + produto2.preco);
+
+        Produto produto3 = new Produto();
+        produto3.codigo = 120;
+        produto3.nome = "Playstation 5";
+        produto3.preco = 4000;
+        produto3.peso = 1000.0;
+        produto3.diminuirPreco(2000);
+        System.out.println("Preço atual do " + produto3.nome + ": " + "R$ " + produto3.preco);
 
     }
-
-    Produto produto2 = new Produto();
-
-    Produto produto3 = new Produto();
-    System.out.println("produto3 - " + produto3.codigo);
 }
